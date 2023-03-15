e2271826d0e33006d7120938b64312dc713d44eb
HHoang <hoanghust218@gmail.com>
2023-03-15 16:31:59 +0700

    admin-cms

---
+++ src/api/product-my-a/routes/product-my-a.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-my-a router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-my-a.product-my-a');
---
+++ src/api/product-my-a/services/product-my-a.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-my-a service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-my-a.product-my-a');
