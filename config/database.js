abb48e63ab54d5613cd875d42c35614900a77704
hoang-viet <thvroyal@gmail.com>
2023-03-25 22:10:09 +0700

    update connection timeout

--- config/database.js
+++ config/database.js
@@ -10,6 +10,17 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
     },
+    acquireConnectionTimeout: 1000000,
+    pool: {
+      min: 0,
+      max: 4,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis:1000,
+      createRetryIntervalMillis: 2000
+    },
     useNullAsDefault: true,
   },
 });
